60e2b8c13fccc36d6dd0b8f031075b47ceb5226b
Karyna <karyna.alyshkevich@gmail.com>
2024-10-23 15:15:23 +0200

    PalindromeNumber with converting into String[]

--- src/PalindromeNumber.java
+++ src/PalindromeNumber.java
@@ -4,36 +4,20 @@ public class PalindromeNumber {
 
     public boolean isPalindrome(int x) {
 
-        try {
-            int[] numbers = convertIntoArrayOfInt(x);
+        String[] numbers = String.valueOf(x).split("");
 
-            int firstDigit = 0;
-            int lastDigit = numbers.length - 1;
+        int firstDigit = 0;
+        int lastDigit = numbers.length - 1;
 
-            while (firstDigit < lastDigit) {
-                if (numbers[firstDigit] == numbers[lastDigit]) {
-                    firstDigit++;
-                    lastDigit--;
-                } else return false;
-            }
-
-        } catch (NumberFormatException ex) {
-            ex.getMessage();
+        while (firstDigit < lastDigit) {
+            if (numbers[firstDigit].equals(numbers[lastDigit])) { // String compares with equals; == compare hashCodes
+                firstDigit++;
+                lastDigit--;
+            } else return false;
         }
         return true;
     }
 
-    public int[] convertIntoArrayOfInt(int x) {
-
-        if (String.valueOf(x).matches("\\d+")) {
-            int[] numbers = Arrays.stream(String.valueOf(x).split(""))
-                    .mapToInt(Integer::parseInt)
-                    .toArray();
-            return numbers;
-        } else {
-            throw new NumberFormatException("Not a valid integer");
-        }
-    }
 
     public static void main (String[] args) {
 
